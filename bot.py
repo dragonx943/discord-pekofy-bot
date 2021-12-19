18b132e6846a5aae0b56bf835ab665bf82a7f1bf
bemxio <bemxiov@protonmail.com>
2021-12-19 14:40:34 +0100

    feat(bot): added basic logging

--- bot.py
+++ bot.py
@@ -2,15 +2,33 @@ from discord.ext import commands
 from discord import Intents
 import logging, os
 
+# setting up logging
+logger = logging.basicConfig(
+    level=logging.INFO,
+    format="(%(asctime)s) [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+    
+    filename="logs/pekolog.log", 
+    encoding="utf-8"
+)
+
 # message content intent is required for reactions (replying to a message with a keyword)
 intents = Intents.default()
 intents.messages = True
 
 bot = commands.Bot(command_prefix=os.environ["PEKOBOT_PREFIX"], intents=intents)
 
+# specific events (mostly for logging)
 @bot.event
 async def on_ready():
-    print(f"logged in as {bot.user}")
+    logging.info(f"Succesfully logged in as {bot.user}!")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    
+    logging.warning(f"A user tried to use {ctx.command} but got an error: {error}")
 
 # loading extensions
 bot.load_extension("jishaku")
@@ -18,3 +36,4 @@ bot.load_extension("extensions.pekofy")
 bot.load_extension("extensions.reactions")
 
 bot.run(os.environ["PEKOBOT_TOKEN"])
+logging.info("Shutting down...\n")
