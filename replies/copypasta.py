35e60e9cf0f1b53993ea4a631f9c7bdf0716cf36
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-01-17 21:18:27 +0530

    bug(replies): replace "*" with "\*" (#18)

--- replies/copypasta.py
+++ replies/copypasta.py
@@ -24,14 +24,14 @@ The way he stared made me feel a bit funny too, but I decided to tease him more 
 He asked me, "Are you serious?" and I said "Yep peko."
 
 He went silent for what seemed like forever, so I asked him, "What's the matter peko?" 
-He said he's confused, but then his b*ner got really hard, which made me take off his clothes. 
-I expected him to scream, "Stop!" as I kissed him and stroked his c*ck, 
-but he instead shouted "Oh God, Pekora!" which made me get a b*ner myself. 
-Before I knew it, I was bl*wing him for the first time till he came.
+He said he's confused, but then his b\*ner got really hard, which made me take off his clothes. 
+I expected him to scream, "Stop!" as I kissed him and stroked his c\*ck, 
+but he instead shouted "Oh God, Pekora!" which made me get a b\*ner myself. 
+Before I knew it, I was bl\*wing him for the first time till he came.
 
-His s*men was so thick, it got stuck inside my throat no matter how hard I swallowed. 
-He then said, "I want to f*ck you now!" and seeing that we've already gone that far and we were both naked, I obliged.
-A few hours later, the jerk went all pale and said to me "Why did we do that? Now I'm not f*cking straight." 
+His s\*men was so thick, it got stuck inside my throat no matter how hard I swallowed. 
+He then said, "I want to f\*ck you now!" and seeing that we've already gone that far and we were both naked, I obliged.
+A few hours later, the jerk went all pale and said to me "Why did we do that? Now I'm not f\*cking straight." 
 But he still looked so cute all confused like that, 
-so I took pity on him and reassured while wiping his c*m off my face, "Let's just pretend I'm still Pekora."
+so I took pity on him and reassured while wiping his c\*m off my face, "Let's just pretend I'm still Pekora."
 """
